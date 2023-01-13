d97c767fa66603ec6540b3bed60883a01cdc3e30
Romanenco <roman.romanenco@disneystreaming.com>
2023-01-13 14:18:08 -0500

    added option to schedule tweets from csv file

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import tweepy
-import schedule
-import time
+import csv
 import argparse
+from datetime import datetime, timedelta
 
 # Twitter API credentials
 consumer_key = "YOUR_CONSUMER_KEY"
@@ -9,27 +9,40 @@ consumer_secret = "YOUR_CONSUMER_SECRET"
 access_token = "YOUR_ACCESS_TOKEN"
 access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
 
-# Connect to the Twitter API using Tweepy
+# Authenticate with Twitter API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Function to schedule and send tweets
-def tweet(message):
-    api.update_status(message)
-    print("Tweeted: " + message)
-
-# Use argparse to accept the tweet message and scheduled time as command line arguments
+# Set up command line flag arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--message", required=True, help="The message to tweet")
-parser.add_argument("-s", "--schedule", required=True, help="The schedule time for the tweet in 24 hour format (HH:MM)")
+parser.add_argument("-s", "--schedule", help="Schedule a single tweet")
+parser.add_argument("-f", "--file", help="Schedule tweets from a CSV file")
+parser.add_argument("-t", "--time", required=True help="Scheduled time for tweets in 24-hour format (HH:MM)")
 args = parser.parse_args()
-message = args.message
-schedule_time = args.schedule
 
-# Schedule the tweet to be sent at the input time
-schedule.every().day.at(schedule_time).do(tweet, message)
+# Schedule a single tweet
+if args.schedule:
+    scheduled_time = datetime.strptime(args.time, "%H:%M")
+    current_time = datetime.now().time()
+    if current_time >= scheduled_time:
+        scheduled_time += timedelta(days=1)
+    scheduled_datetime = datetime.combine(datetime.now(), scheduled_time)
+    api.update_status(args.schedule, scheduled_datetime=scheduled_datetime)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# Schedule tweets from a CSV file
+elif args.file:
+    with open(args.file, 'r') as csvfile:
+        tweetreader = csv.reader(csvfile)
+        tweets = [row[0] for row in tweetreader]
+    scheduled_time = datetime.strptime(args.time, "%H:%M")
+    current_time = datetime.now().time()
+    if current_time >= scheduled_time:
+        scheduled_time += timedelta(days=1)
+    scheduled_datetime = datetime.combine(datetime.now(), scheduled_time)
+    for tweet in tweets:
+        api.update_status(tweet, scheduled_datetime=scheduled_datetime)
+        scheduled_datetime += timedelta(days=1)
+
+else:
+    print("Please provide a tweet or file to schedule")
